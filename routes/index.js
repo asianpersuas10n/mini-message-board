4740d287ae99f5c2e8cda64a1b39572768fef614
nick <nick.blaeds@gmail.com>
2023-09-06 17:23:32 -0500

    Allow users to create and post a message at /new

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,28 @@
 var express = require("express");
 var router = express.Router();
+const messages = [
+  {
+    text: "hello world",
+    user: "me",
+    added: new Date(),
+  },
+  {
+    text: "hi there",
+    user: "test",
+    added: new Date(),
+  },
+];
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  const messages = [
-    {
-      text: "hello world",
-      user: "me",
-      added: new Date(),
-    },
-    {
-      text: "hi there",
-      user: "test",
-      added: new Date(),
-    },
-  ];
   res.render("index", { title: "Mini Messageboard", messages: messages });
 });
 
 // Post from new message form
-router.post("/new");
+router.post("/new", (req, res) => {
+  const { message, name } = req.body;
+  messages.push({ text: message, user: name, added: new Date() });
+  res.redirect("/");
+});
 
 module.exports = router;
